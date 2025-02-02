f2ae25a563d446f0a1333cd81d5bfae2f237a5f4
Fernando Butzke <fernandobutzke1@gmail.com>
2025-02-01 21:30:59 -0300

    feat: Adding kafka producer

--- producer/src/app.service.ts
+++ producer/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9093'],
+      },
+      consumer: {
+        groupId: 'ec-consumer',
+      },
+    },
+  })
+  client: ClientKafka;
+
+  sendToStorage() {
+    return this.client.emit('storage-ec', {
+      message: 'remove item from storage',
+    });
   }
 }
